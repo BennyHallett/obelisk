38f7fac3149cee55e4aedff1b62a86e5d30918c2
BennyHallett <benny.hallett@gmail.com>
2014-07-09 08:48:05 +1000

    0.0.1

--- mix.exs
+++ mix.exs
@@ -5,26 +5,27 @@ defmodule Obelisk.Mixfile do
     [app: :obelisk,
      version: "0.0.1",
      elixir: "~> 0.14.1",
+     package: package,
+     docs: [readme: true, main: "README.md"],
+     description: """
+      obelisk is a static site generator for Elixir. It is inspired by jekyll,
+      with the goal of being fast and simple to use and extend.
+     """,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: []]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:markdown, github: "devinus/markdown"}]
+  end
+
+  defp package do
+    %{
+      licences: ["MIT"],
+      links: %{ "Github" => "https://github.com/bennyhallett/obelisk"}
+    }
   end
 end
