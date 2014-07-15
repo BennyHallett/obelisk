0d897389d2fecd080e3cc7fa05fdd925734eaf0c
BennyHallett <benny.hallett@gmail.com>
2014-07-16 08:45:54 +1000

    Parsing out front matter before processing

--- lib/obelisk/post.ex
+++ lib/obelisk/post.ex
@@ -2,10 +2,11 @@ defmodule Obelisk.Post do
 
   def compile(md_file) do
     { :ok, md } = File.read("./posts/#{md_file}")
+    { frontmatter, md_content } =  parts(md)
     { :ok, template } = File.read("./layout/post.eex")
     { :ok, layout } = File.read("./layout/layout.eex")
     File.write(html_filename(md_file),
-      EEx.eval_string(layout, assigns: [js: Obelisk.Assets.js, css: Obelisk.Assets.css, content: EEx.eval_string(template, assigns: [content: Markdown.to_html(md)])]))
+      EEx.eval_string(layout, assigns: [js: Obelisk.Assets.js, css: Obelisk.Assets.css, content: EEx.eval_string(template, assigns: [content: Markdown.to_html(md_content)])]))
   end
 
   def html_filename(md) do
@@ -18,6 +19,11 @@ defmodule Obelisk.Post do
 
   def list do
     File.ls! "./posts"
+  end
+
+  def parts(page_content) do
+    [frontmatter|content] = String.split page_content, "\n---\n"
+    { frontmatter, Enum.join(content, "\n") }
   end
 
 end
--- test/post_test.exs
+++ test/post_test.exs
@@ -1,13 +1,31 @@
 defmodule PostTest do
   use ExUnit.Case
 
-  setup do
-  end
-
   test "Can properly parse md filename to post name" do
     filename = "2014-02-03-this-is-a-test.markdown"
     processed = Obelisk.Post.title(filename)
     assert processed == "This is a test"
+  end
+
+  test "separate post into front matter and content" do
+    { frontmatter, content } = Obelisk.Post.parts(post_content)
+    assert "---\nfront: matter\noh: yeah" == frontmatter
+    assert "\nThis is the post content\n\n* And\n* A\n* List\n" == content
+  end
+
+  defp post_content do
+    """
+    ---
+    front: matter
+    oh: yeah
+    ---
+
+    This is the post content
+
+    * And
+    * A
+    * List
+    """
   end
 
 end
