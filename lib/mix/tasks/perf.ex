81277501caac1dcc8eb07dd8b34d74666d0ee701
BennyHallett <benny.hallett@gmail.com>
2014-09-17 09:16:11 +1000

    Moved the performance test stuff into its own mix task

--- lib/mix/tasks/perf.ex
+++ lib/mix/tasks/perf.ex
@@ -1,18 +1,18 @@
-defmodule PerformanceTest do
-  use ExUnit.Case
+defmodule Mix.Tasks.Perf do
+  use Mix.Task
 
-  setup do
-    TestHelper.cleanup
-    on_exit fn -> TestHelper.cleanup end
+  @shortdoc "Run obelisk performance benchmarks."
+  @moduledoc """
+  Runs obelisk performance benchmarks.
+  """
+  def run(args) do
+    Obelisk.Tasks.Init.run([])
+    make_10k_posts
+    IO.puts Chronos.Formatter.strftime(Chronos.now, "%Y-%0m-%0d-%H:%M:%S")
+    Obelisk.Tasks.Build.run([])
+    IO.puts Chronos.Formatter.strftime(Chronos.now, "%Y-%0m-%0d-%H:%M:%S")
+    cleanup
   end
-
-#  test "Build task compiles posts into the build dir" do
-#    Obelisk.Tasks.Init.run([])
-#    make_10k_posts
-#    IO.puts Chronos.Formatter.strftime(Chronos.now, "%Y-%0m-%0d-%H:%M:%S")
-#    Obelisk.Tasks.Build.run([])
-#    IO.puts Chronos.Formatter.strftime(Chronos.now, "%Y-%0m-%0d-%H:%M:%S")
-#  end
 
   defp make_10k_posts do
     1..10_000 |> Enum.each &(create_post &1)
@@ -67,6 +67,16 @@ defmodule PerformanceTest do
 
     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
     """
+  end
+
+  defp cleanup do
+    File.rm_rf "./assets"
+    File.rm_rf "./posts"
+    File.rm_rf "./drafts"
+    File.rm_rf "./pages"
+    File.rm_rf "./layout"
+    File.rm "./site.yml"
+    File.rm_rf "./build"
   end
 
 end
