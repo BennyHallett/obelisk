fce6c2c8e718b87acf2465c2d64e349082e83ed9
BennyHallett <benny.hallett@gmail.com>
2015-01-26 10:57:04 +1100

    Bumped version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Obelisk.Mixfile do
 
   def project do
     [app: :obelisk,
-     version: "0.7.1",
+     version: "0.8.0",
      elixir: "~> 1.0.0",
      package: package,
      docs: [readme: true, main: "README.md"],
