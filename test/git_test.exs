62a32a52f27813b09b85ad354dc56579f7848e06
BennyHallett <benny.hallett@gmail.com>
2015-02-06 08:04:06 +1100

    Finished testing git cloning

--- test/git_test.exs
+++ test/git_test.exs
@@ -1,5 +1,6 @@
 defmodule GitTest do
   use ExUnit.Case
+  import Mock
 
   setup do
     Obelisk.Config.force %{ theme: "default", posts_per_page: 5 }
@@ -9,6 +10,13 @@ defmodule GitTest do
   test "Parse git url properly" do
     url = "http://github.com/test/repo.git"
     assert { url, "repo" } == Obelisk.Git.parse_url(url)
+  end
+
+  test "Clone repo" do
+    with_mock System, [cmd: fn(_cmd, _args) -> "cloned" end] do
+      Obelisk.Git.clone("http://example.com/user/repo.git")
+      assert called System.cmd("git", ["clone", "http://example.com/user/repo.git", "./themes/repo"])
+    end
   end
 
 end
