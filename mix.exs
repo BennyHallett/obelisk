f14f2dcf3d252780830fad5c683c1756b3cd332c
BennyHallett <benny.hallett@gmail.com>
2015-03-05 08:16:40 +1100

    v0.9.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Obelisk.Mixfile do
 
   def project do
     [app: :obelisk,
-     version: "0.9.0",
+     version: "0.9.1",
      elixir: "~> 1.0.0",
      package: package,
      docs: [readme: true, main: "README.md"],
