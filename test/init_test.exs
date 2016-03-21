4e866647a06cb4ec65702c3a0efd4e117ab59241
Zeger-Jan van de Weg <zegerjan@gitlab.com>
2016-03-21 13:16:44 +0100

    Update gitignore on an new project

--- test/init_test.exs
+++ test/init_test.exs
@@ -2,43 +2,40 @@ defmodule InitTaskTest do
   use ExUnit.Case, async: false
 
   setup do
+    Obelisk.Site.test_setup(System.tmp_dir)
+
     on_exit fn -> TestHelper.cleanup end
   end
 
   test "Init task creates assets directory structure" do
-    Obelisk.Tasks.Init.run([])
+    dirs = ~w(./themes ./themes/default ./themes/default/assets ./themes/default/assets/css
+        ./themes/default/assets/js ./themes/default/assets/img ./themes/default/assets/css/base.css
+        ./themes/default/layout)
+    Enum.each(dirs, fn(dir) -> File.dir? dir |> assert end)
 
-    assert File.dir? "./themes"
-    assert File.dir? "./themes/default"
-    assert File.dir? "./themes/default/assets"
-    assert File.dir? "./themes/default/assets/css"
-    assert File.dir? "./themes/default/assets/js"
-    assert File.dir? "./themes/default/assets/img"
-    assert File.exists? "./themes/default/assets/css/base.css"
-
-    assert File.dir? "./themes/default/layout"
-    assert File.exists? "./themes/default/layout/layout.eex"
-    assert File.exists? "./themes/default/layout/post.eex"
-    assert File.exists? "./themes/default/layout/page.eex"
+    files = ~w(./themes/default/layout/layout.eex ./themes/default/layout/post.eex
+    ./themes/default/layout/page.eex)
+    Enum.each(files, fn(file) -> File.exists?(file) |> assert end)
   end
 
   test "Init task creates content directory structure" do
-    Obelisk.Tasks.Init.run([])
-
     assert File.dir? "./posts"
     assert File.dir? "./drafts"
     assert File.dir? "./pages"
   end
 
   test "Init task creates initial config file" do
-    Obelisk.Tasks.Init.run([])
-
     assert File.exists? "./site.yml"
   end
 
   test "Init task creates first post" do
-    Obelisk.Tasks.Init.run([])
-
     assert File.exists? "./posts/#{TestHelper.datepart}-welcome-to-obelisk.markdown"
+  end
+
+  test "appends the build dir to the .gitignore" do
+    assert File.exists?(".gitignore")
+    { :ok, file } = File.read(".gitignore")
+
+    assert String.match?(file, ~r/build\n\z/)
   end
 end
