203633041506aed6b77a221d3f40ff9d6df2af33
James Wheaton <jwheaton@kpmg.com>
2017-01-30 15:20:48 +0900

    Update anubis dependency to 0.3.0

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Obelisk.Mixfile do
      {:cowboy, "~> 1.0.0"},
      {:plug, "~> 0.11.1"},
      {:rss, "~> 0.2.1"},
-     {:anubis, "~> 0.1.0"},
+     {:anubis, "~> 0.3.0"},
      {:mock, "~> 0.1.0"},
      {:calliope, "~> 0.3.0"}]
   end
