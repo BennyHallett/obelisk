c9d90699d9bbdeb7f179daed80d17f0a229da7eb
MinasMazar <minasmazar@gmail.com>
2019-01-05 22:56:36 +0100

    Ensure obelisk render to include `site.yml` content as global assign
    Now you can do `<a href="<%= @site.url %>"><%= @site.name %></a>`

--- lib/obelisk/renderer.ex
+++ lib/obelisk/renderer.ex
@@ -2,7 +2,11 @@ defmodule Obelisk.Renderer do
 
   def render(content, format), do: render(content, [], format)
 
-  def render(content, params, :eex), do: EEx.eval_string(content, assigns: params)
-  def render(content, params, :haml), do: Calliope.render(content, params)
+  def render(content, params, :eex), do: EEx.eval_string(content, assigns: ensure_global(params))
+  def render(content, params, :haml), do: Calliope.render(content, ensure_global(params))
 
+  def ensure_global(params) do
+    params
+    |> Keyword.merge([site: Obelisk.Config.config])
+  end
 end
